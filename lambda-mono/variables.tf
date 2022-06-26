72da2cfaf284a4805556fa08391c9f5344c3e397
Test <tech@gmail.com>
2022-06-26 17:16:20 +0200

    Removing TFVARS file to make it even more monolithic.

--- lambda-mono/variables.tf
+++ lambda-mono/variables.tf
@@ -9,5 +9,16 @@ variable "horizontal_api" {
 
 variable "services_list" {
   type = list
-  default = []
+  default = [
+    {
+      lambda_func_name  = "func-internal"
+      endpoint_to_probe = "https://www.google.com"
+      spoc_service_name = "internal_xyz_service"
+    },
+    {
+      lambda_func_name  = "func-external"
+      endpoint_to_probe = "https://www.msn.com"
+      spoc_service_name = "external_xyz_service"
+    }
+  ]
 }
